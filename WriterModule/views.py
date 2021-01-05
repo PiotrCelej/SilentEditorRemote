b1dbaf24606d3c921c98ee5b83c49e512ebe76cf
Piotr Celej <piotrcelej@interia.pl>
2021-01-05 11:03:08 +0100

    Check from VSC

--- WriterModule/views.py
+++ WriterModule/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. And this comment is to check
 def index(request) :
     return HttpResponse('Main Writer Module Page')
