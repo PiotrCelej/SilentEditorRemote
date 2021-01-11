b5f43feb4d8463dcb3efa99054e593ac9119af34
Piotr Celej <piotrcelej@interia.pl>
2021-01-11 14:32:45 +0100

    Models introduced

--- WriterModule/models.py
+++ WriterModule/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MainTextBody(models.Model) :
+    body_text = models.TextField()
+    last_update_date = models.DateTimeField('last update')
+    author = models.CharField(max_length=512)
