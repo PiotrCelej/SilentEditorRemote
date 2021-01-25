e4bc63726d78879a3f27efbc7419879c35466ca2
Piotr Celej <piotrcelej@interia.pl>
2021-01-25 13:21:00 +0100

    All views to show data prepared

--- WriterModule/views.py
+++ WriterModule/views.py
@@ -7,26 +7,29 @@ from .models import MainTextBody
 
 # Create your views here. And this comment is to check
 def index(request) :
-    doc_list = MainTextBody.objects.order_by('-last_update_date')
-    print (doc_list)
     context = {
-        'doc_list' : doc_list
     }
     return render(request, 'WriterModule/index.html', context)
 
 def docInfo(request, doc_id) :
     doc_list = MainTextBody.objects.filter(text_id=doc_id)
-    print (doc_list)
     doc_data = doc_list[0]
     return HttpResponse(str(doc_data))
 
 def docList(request, user_name) :
     doc_list = MainTextBody.objects.filter(author=user_name).order_by('-last_update_date')
-    print (doc_list)
     context = {
-        'doc_list' : doc_list
+        'doc_list' : doc_list,
+        'page_title' : "Writer Module Doc List"
     }
     return render(request, 'WriterModule/doc_list.html', context)
 
 def docRead(request, doc_id) :
-    return HttpResponse("Showing document: " + str(doc_id))
+    doc_list = MainTextBody.objects.filter(text_id=doc_id)
+    doc_data = doc_list[0]
+    context = {
+        'doc_name' : doc_data.name,
+        'doc_date' : doc_data.last_update_date,
+        'doc_text' : doc_data.body_text
+    }
+    return render(request, 'WriterModule/doc_view.html', context)
