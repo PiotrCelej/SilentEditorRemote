2385615055f940b810d1ad8054a2d5a48f11a0a6
Piotr Celej <piotrcelej@interia.pl>
2021-03-24 09:19:59 +0100

    Dodanie strony głównej z linkami do modułów

---
+++ SilentEditor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SilentEditorConfig(AppConfig):
+    name = 'SilentEditor'
--- SilentEditor/settings.py
+++ SilentEditor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SilentEditor.apps.SilentEditorConfig',
     'WriterModule.apps.WritermoduleConfig',
     'ParagraphModule.apps.ParagraphModuleConfig',
     'django_summernote',
