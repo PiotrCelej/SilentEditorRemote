80ada4c812e35e2ced9835a8be68206396155ebd
Piotr Celej <piotrcelej@interia.pl>
2021-09-14 22:36:12 +0200

    Modele paragrafów i powiązań

--- ParagraphModule/models.py
+++ ParagraphModule/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Paragraph(models.Model) :
+    par_id = models.CharField(max_length=100)
+    par_body = models.TextField()
+    par_is_root = models.BooleanField(default=False)
+    par_is_end = models.BooleanField(default=False)
+
+    def __str__(self) -> str :
+        text_to_show = "[" + self.par_id +";"+ self.par_body +";"+ self.par_is_root +";"+ self.par_is_end +"]"
+        return text_to_show
+
+    def getParBody(self) :
+        return self.par_body
+
+    def getParId(self) :
+        return self.par_id
+
+class ParagraphLinks(models.Model) :
+    par_from_id = models.CharField(max_length=100)
+    par_from_text = models.TextField
+    par_to_id = models.CharField(max_length=100)
+
+    def __str__(self) -> str :
+        text_to_show = "["+ self.par_from_id +";"+ self.par_from_text +";"+ self.par_to_id +"]"
+        return text_to_show
+
+    def getFromText(self) :
+        return self.par_from_text
