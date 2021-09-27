647989bb85c5cc11732e91e8d8bb0bd6f78a38a6
Piotr Celej <piotrcelej@interia.pl>
2021-09-27 16:01:42 +0200

    Paragraph edit workflow

--- ParagraphModule/views.py
+++ ParagraphModule/views.py
@@ -11,23 +11,24 @@ def index(request) :
 
 def paragraphList(request, user_name) :
     par_list = Paragraph.objects.all()
+    print(par_list)
     context = {
         'par_list' : par_list,
         'page_title' : "Paragraphs"
     }
     return render(request, 'ParagraphModule/par_list.html', context)
 
-def paragraphEdit(request, user_name) :
+def paragraphEdit(request) :
     submitted = False
     if request.method == 'POST':
         form = ParagraphEditForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('paragraph/addParagraph?submitted=True')
+            return HttpResponseRedirect('/paragraph/viewParagraph?submitted=True')
     else:
         form = ParagraphEditForm()
         if 'submitted' in request.GET:
             submitted = True
-    return render(request, 'ParagraphModule/par_add.html', 
+    return render(request, 'ParagraphModule/par_view.html', 
             {'form' : form, 'submitted' : submitted})
 
