c0e909c6eaa1c099c774bff482f214c771100f4c
Piotr Celej <piotrcelej@interia.pl>
2021-10-13 11:24:58 +0200

    Modele dla paragrafówek

--- ParagraphModule/models.py
+++ ParagraphModule/models.py
@@ -1,8 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model) :
+    str_id = models.CharField(max_length=100)
+    str_title = models.CharField
+    str_author = models.CharField
+    str_create_date = models.DateField
+    str_last_update_date = models.DateField
+
+    def __str__(self) -> str :
+        text_to_show = "[" + self.str_id + "," + self.str_title + "," + self.str_author + "," + str(self.str_create_date) + "," + str(self.str_last_update_date) + "]"
+        return text_to_show
+
+class Character(models.Model) :
+    chr_id = models.CharField(max_length=100)
+    chr_str_id = models.CharField
+    chr_name = models.CharField
+    chr_type = models.CharField
+    chr_story = models.TextField
+    chr_image = models.ImageField
+
+    def __str__(self) -> str :
+        text_to_show = "[" + self.chr_id + "," + self.chr_name + "," + self.chr_type + "," + self.chr_story + "]"
+        return text_to_show
+
+class Item(models.Model) :
+    itm_id = models.CharField(max_length=100)
+    itm_str_id = models.CharField
+    itm_name = models.CharField
+    itm_description = models.TextField
+    itm_image = models.ImageField
+
+    def __str__(self) -> str :
+        text_to_show = "[" + self.itm_id + "," + self.itm_name + "," + self.itm_description + "]"
+        return text_to_show
+
 class Paragraph(models.Model) :
     par_id = models.CharField(max_length=100)
+    par_str_id = models.CharField
     par_body = models.TextField()
     par_is_root = models.BooleanField(default=False)
     par_is_end = models.BooleanField(default=False)
@@ -28,13 +63,4 @@ class ParagraphLinks(models.Model) :
 
     def getFromText(self) :
         return self.par_from_text
-
-class Character(models.Model) :
-    char_id = models.CharField(max_length=100)
-
-    # types of characters:
-    # NPC - npc which interacts with our hero
-    # HERO - a hero managed by palyer
-    char_type = models.CharField(max_length=20)
-    char_name = models.CharField(max_length=100)
     
